557d42463d8ce03762cce004878794ece5d52869
Andre S. Viana <andre.silva.viana.1995@gmail.com>
2026-03-28 16:27:21 -0300

    Segundo commit - criado o personagem e o rastro

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,27 @@ public class PlayerController : MonoBehaviour
     public float speed = 5f;
     private Vector2 move;
 
+    public GameObject trailPrefab;
+    private float trailTimer = 0.1f;
+
     void Update()
     {
         move.x = Input.GetAxisRaw("Horizontal");
         move.y = Input.GetAxisRaw("Vertical");
 
         transform.Translate(move.normalized * speed * Time.deltaTime);
+
+        CreateTrail();
+    }
+
+    void CreateTrail()
+    {
+        trailTimer -= Time.deltaTime;
+
+        if (trailTimer <= 0)
+        {
+            Instantiate(trailPrefab, transform.position, Quaternion.identity);
+            trailTimer = 0.1f;
+        }
     }
 }
